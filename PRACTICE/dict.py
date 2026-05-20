0d8efbe8bcdeed029d7675f3c54b5d43f95752fd
aditya011-dev <adityasharmaa011@gmail.com>
2026-05-21 01:00:52 +0530

    remove PRACTICE folder from tracking

--- PRACTICE/dict.py
+++
@@ -1,4 +0,0 @@
-marks = {"Rohan": 85, "Sohan": 90, "Mohan": 78, "Gita": 92, "Raj": 88}
-print(marks , type(marks))  # Output: {'Rohan': 85, 'Sohan': 90, 'Mohan': 78, 'Gita': 92, 'Raj': 88} <class 'dict'>
-print (marks["Rohan"])  # Output: 85
-print (marks["Sohan"])  # Output: 90
